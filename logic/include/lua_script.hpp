eb62ccac6f89ced1b13568b34572712d6984c60f
Olle@School <olle_sz@hotmail.com>
2018-09-05 10:58:35 +0200

    Added luascript to logic

---
+++ logic/include/lua_script.hpp
@@ -0,0 +1,41 @@
+#ifndef LUA_SCRIPT_H
+#define LUA_SCRIPT_H
+#include <string>
+#include <vector>
+#include <iostream>
+//::.. authors ..:://
+// Olle
+// Edvard
+/*
+class LuaScript
+{
+public:
+	LuaScript(const std::string& filename);
+	~LuaScript();
+
+	void print_error(const std::string& variable_name, const std::string& reason);
+	template<typename T>
+	T get(const std::string& variable_name)
+	{
+		// will be implemented later in tutorial
+	}
+	bool get_to_stack(const std::string& variable_name)
+	{
+		// will be explained later too
+	}
+	// Generic get
+	template<typename T>
+	T get(const std::string& variable_name)
+	{
+		return 0;
+	}
+	// Generic default get
+	template<typename T>
+	T get_default(const std::string& variable_name) {
+		return 0;
+	}
+private:
+	lua_State* L;
+};
+*/
+#endif
