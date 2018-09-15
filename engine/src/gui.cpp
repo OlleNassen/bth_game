713476445f181c8d8535bde97c52f1af8b31f090
Deadvard <edvardleiborn@telia.com>
2018-09-15 15:34:12 +0200

    added enter to open chat

--- engine/src/gui.cpp
+++ engine/src/gui.cpp
@@ -7,6 +7,7 @@ namespace gui
 using namespace std::chrono_literals;
 
 std::string string_buffer;
+bool chat_on = false;
 
 const std::string& text_buffer()
 {
@@ -26,6 +27,7 @@ void key_callback(GLFWwindow* window, int key, int scancode, int action, int mod
 	}
 	else if (action == GLFW_PRESS && key == GLFW_KEY_ENTER)
 	{
+		chat_on = !chat_on;
 		string_buffer.clear();
 	}	
 }
@@ -33,31 +35,36 @@ void key_callback(GLFWwindow* window, int key, int scancode, int action, int mod
 void chat::update(std::chrono::milliseconds delta)
 {
 	constexpr auto underscore = '_';
-
+	
 	if (chat_log[0].size() > 1 && string_buffer.empty())
 	{
 		if (has_underscore)
 		{
 			chat_log[0].pop_back();
 			chat_log[1] = chat_log[0];
-		}	
+		}
 		else
 		{
 			chat_log[1] = chat_log[0];
-		}		
+		}
 	}
-	
+
 	if (has_underscore)
 		chat_log[0] = string_buffer + underscore;
 	else
 		chat_log[0] = string_buffer;
-	
+
 	time += delta;
 
 	if (time > 500ms)
 	{
 		has_underscore = !has_underscore;
 		time = 0ms;
+	}
+
+	if (!chat_on)
+	{
+		chat_log[0].clear();
 	}
 }
 
