50225a89ca22b5b0a25bf4ec9cfe6629092abbce
Olle@School <olle_sz@hotmail.com>
2018-09-19 11:19:20 +0200

    big fix

--- graphics/src/mesh.cpp
+++ graphics/src/mesh.cpp
@@ -26,40 +26,75 @@ Mesh::Mesh(const std::string meshfile)
 	string filepath = "../resources/assets/" + meshfile;
 	custom_mesh = new CustomMesh(filepath.c_str());
 
+	custom_mesh->vertices->tx;
+	custom_mesh->vertices->ty;
+	custom_mesh->vertices->tz;
+
+
 	glGenVertexArrays(1, &vao_id);
 	glBindVertexArray(vao_id);
 	glEnableVertexAttribArray(0);
 	glEnableVertexAttribArray(1);
 	glEnableVertexAttribArray(2);
+	glEnableVertexAttribArray(3);
+	glEnableVertexAttribArray(4);
+	glEnableVertexAttribArray(5);
+	glEnableVertexAttribArray(6);
 
 	glGenBuffers(1, &vbo_id);
 	glBindBuffer(GL_ARRAY_BUFFER, vbo_id);
-	glBufferData(GL_ARRAY_BUFFER, custom_mesh->counterReader.vertexCount * sizeof(VertexInformation), &custom_mesh->vertices[0], GL_STATIC_DRAW);
-
+	glBufferData(GL_ARRAY_BUFFER, 
+		custom_mesh->counterReader.vertexCount * sizeof(VertexInformation), 
+		custom_mesh->vertices, GL_STATIC_DRAW);
+	
+	//position
 	glVertexAttribPointer(
-		0,
-		3,
-		GL_FLOAT,
-		GL_FALSE,
+		0, 3,
+		GL_FLOAT, GL_FALSE,
 		sizeof(VertexInformation),
-		BUFFER_OFFSET(0)
-	);
-
+		BUFFER_OFFSET(0));
+	
+	//normal
 	glVertexAttribPointer(
-		1,
-		3,
-		GL_FLOAT,
-		GL_FALSE, sizeof(VertexInformation),
-		BUFFER_OFFSET(sizeof(float) * 3)
-	);
-
+		1, 3, 
+		GL_FLOAT, GL_FALSE, 
+		sizeof(VertexInformation),
+		BUFFER_OFFSET(sizeof(float) * 3));
+	
+	//binormal
 	glVertexAttribPointer(
-		2,
-		2,
-		GL_FLOAT,
-		GL_FALSE, sizeof(VertexInformation),
-		BUFFER_OFFSET(sizeof(float) * 6)
-	);
+		2, 3,
+		GL_FLOAT, GL_FALSE, 
+		sizeof(VertexInformation),
+		BUFFER_OFFSET(sizeof(float) * 6));
+
+	//tangent
+	glVertexAttribPointer(
+		3, 3,
+		GL_FLOAT, GL_FALSE, 
+		sizeof(VertexInformation),
+		BUFFER_OFFSET(sizeof(float) * 9));
+
+	//uv
+	glVertexAttribPointer(
+		4, 2,
+		GL_FLOAT, GL_FALSE, 
+		sizeof(VertexInformation),
+		BUFFER_OFFSET(sizeof(float) * 11));
+
+	//weights
+	glVertexAttribPointer(
+		5, 4,
+		GL_FLOAT, GL_FALSE, 
+		sizeof(VertexInformation),
+		BUFFER_OFFSET(sizeof(float) * 15));
+
+	//weight's id
+	glVertexAttribPointer(
+		6, 4,
+		GL_FLOAT, GL_FALSE, 
+		sizeof(VertexInformation),
+		BUFFER_OFFSET(sizeof(float) * 19));
 }
 
 Mesh::~Mesh()
--- resources/shaders/template.vs
+++ resources/shaders/template.vs
@@ -1,7 +1,11 @@
 #version 440
 layout(location = 0) in vec3 vertex_position;
 layout(location = 1) in vec3 vertex_normal;
-layout(location = 2) in vec2 uv_coord;
+layout(location = 2) in vec3 bi_normal;
+layout(location = 3) in vec3 tangent;
+layout(location = 4) in vec2 uv;
+layout(location = 5) in vec4 weights;
+layout(location = 6) in vec4 weights_id;
 
 uniform mat4 model;
 uniform mat4 view;
@@ -18,6 +22,8 @@ void main()
 	gl_Position = projection * view * model * vec4(vertex_position, 1);
 
 	vs_out.vertex_normal = model * normalize(vec4(vertex_normal, 0));
-	vs_out.tex_coords = uv_coord;
-	vs_out.frag_pos = model * gl_Position;
+
+
+	vs_out.tex_coords = uv;
+	vs_out.frag_pos = model * vec4(vertex_position, 1);
 }
