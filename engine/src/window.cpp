7f601e0d9a1952a604c54e0b6206a97fcb993b97
Olle@School <olle_sz@hotmail.com>
2018-09-21 12:52:52 +0200

    cool

--- engine/src/window.cpp
+++ engine/src/window.cpp
@@ -22,7 +22,7 @@ Window::Window(const glm::ivec2& window_size, const std::string& title)
 	}
 
 	glfwMakeContextCurrent(glfw_window);
-	//glfwSetInputMode(glfw_window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+	glfwSetInputMode(glfw_window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
 	glfwSetCharCallback(glfw_window, gui::character_callback);
 	glfwSetKeyCallback(glfw_window, gui::key_callback);
 
