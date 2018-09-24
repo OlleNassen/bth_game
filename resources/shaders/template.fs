fe152a683354714946baa29f0602a24134d7be63
Olle@School <olle_sz@hotmail.com>
2018-09-24 14:30:12 +0200

    temp

--- resources/shaders/template.fs
+++ resources/shaders/template.fs
@@ -3,7 +3,6 @@
 out vec4 frag_color;
 
 in VS_OUT{
-	vec4 temp_normal;
 	vec3 frag_pos;
 	vec2 tex_coord;
 	vec3 tangent_light_pos;
@@ -13,22 +12,31 @@ in VS_OUT{
 
 uniform vec3 view_pos;
 
+uniform sampler2D diffuse_map;
+uniform sampler2D normal_map;
+
 void main()
 {
-	vec3 normal = normalize(vec3(fs_in.temp_normal.xyz));
-	vec3 ambient = vec3(0.0, 0.0, 0.0);
-	vec3 lighting = vec3(0.0);
-	vec3 view_dir = normalize(view_pos - vec3(fs_in.frag_pos.xyz));
+	vec3 normal = texture(normal_map, fs_in.texcoord).rgb;
+    // transform normal vector to range [-1,1]
+    normal = normalize(normal * 2.0 - 1.0);  // this normal is in tangent space
 
-	vec3 light_dir = normalize(vec3(10, 5, 1) - vec3(fs_in.frag_pos.xyz));
-	float diff = max(dot(light_dir, vec3(fs_in.temp_normal.xyz)), 0.0);
-	vec3 result = vec3(0.9, 0.9, 0.9) * diff * vec3(0.1, 0.5, 0.2);
+    vec3 color = texture(diffuse_map, fs_in.texcoord).rgb;
 
-	float distance = length(vec3(fs_in.frag_pos.xyz) - vec3(10, 5, 1)) / 100;
-	result *= 1.0 / (distance * distance);
-	lighting = result;
+    vec3 ambient = 0.1 * color;
 
-	result = ambient + lighting;
+    vec3 light_direction =
+        normalize(fs_in.tangent_light_pos - fs_in.tangent_fragment_pos);
 
-	frag_color = vec4(result, 1.0);
+    float diff_value = max(dot(light_direction, normal), 0.0);
+    vec3 diffuse = diff_value * color;
+
+    vec3 view_direction =
+        normalize(fs_in.tangent_view_pos - fs_in.tangent_fragment_pos);
+
+    vec3 halfway_direction = normalize(light_direction + view_direction);
+    float spec = pow(max(dot(normal, halfway_direction), 0.0), 32.0);
+    vec3 specular = vec3(0.2) * spec;
+
+frag_color = vec4(ambient + diffuse + specular, 1.0);
 }
