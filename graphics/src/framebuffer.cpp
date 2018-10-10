04ab9acc45fe08e03a5b0edf000e00febec3b091
Deadvard <edvardleiborn@telia.com>
2018-10-10 16:04:03 +0200

    ez fix

--- graphics/src/framebuffer.cpp
+++ graphics/src/framebuffer.cpp
@@ -15,7 +15,6 @@ Framebuffer::Framebuffer()
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);  // we clamp to the edge as the blur filter would otherwise sample repeated texture values!
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-	glBindTexture(GL_TEXTURE_2D, 0);
 	glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, fbo_texture, 0);
 
 	glGenTextures(1, &depth_texture);
@@ -25,13 +24,7 @@ Framebuffer::Framebuffer()
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-	glBindTexture(GL_TEXTURE_2D, 1);
 	glFramebufferTexture2D(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT , GL_TEXTURE_2D, depth_texture, 0);
-
-	/*glGenRenderbuffers(1, &fbo_depth);
-	glBindRenderbuffer(GL_RENDERBUFFER, fbo_depth);
-	glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH24_STENCIL8, 1280, 720);
-	glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_RENDERBUFFER, fbo_depth);*/
 
 	// finally check if framebuffer is complete
 	if (glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE)
@@ -53,12 +46,12 @@ void Framebuffer::bind_framebuffer() const
 void Framebuffer::bind_texture() const
 {
 	glActiveTexture(GL_TEXTURE0);
-	glBindTexture(GL_TEXTURE_2D, depth_texture);
+	glBindTexture(GL_TEXTURE_2D, fbo_texture);
 }
 
 void Framebuffer::bind_depth() const
 {
 	glActiveTexture(GL_TEXTURE1);
-	glBindTexture(GL_TEXTURE_2D, fbo_texture);
+	glBindTexture(GL_TEXTURE_2D, depth_texture);
 }
 }
