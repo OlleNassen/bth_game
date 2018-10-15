4f5e1937fd062b959422ab7d8d837b9c1eb85b44
Olle@School <olle_sz@hotmail.com>
2018-10-15 14:48:54 +0200

    edvard's helpers suck

--- graphics/include/minimap.hpp
+++ graphics/include/minimap.hpp
@@ -7,22 +7,27 @@
 
 namespace graphics
 {
-	class Minimap
-	{
-	public:
-		Minimap();
-		~Minimap();
-		void update(const std::vector<Model>& models, int players);
-		void render(const Shader& shader) const;
-		//std::vector<float> positions;
-		float positions[8];
-	private:
-		//Shader minimap_shader;
-		VertexArray vao_id;
-		Buffer vbo_id;
-		unsigned int height = 100;
-		unsigned int width = 10;
-	};
+
+class Minimap
+{
+public:
+	Minimap();
+	~Minimap() = default;
+	void update(const std::vector<Model>& models, int players);
+	void render(const Shader& shader) const;
+	//std::vector<float> positions;
+	float positions[8];
+private:
+	//Shader minimap_shader;
+	unsigned int vao_id;
+	unsigned int vbo_id;
+
+	//VertexArray vao_id;
+	//Buffer vbo_id;
+	unsigned int height = 100;
+	unsigned int width = 10;
+};
+
 }
 #endif // !MINIMAP_HPP
 
--- graphics/src/minimap.cpp
+++ graphics/src/minimap.cpp
@@ -1,34 +1,36 @@
 #include "minimap.hpp"
-
-
-graphics::Minimap::Minimap()
+#include <gl/glew.h>
+namespace graphics
 {
-	glBindVertexArray(vao_id);
-	glBindBuffer(GL_ARRAY_BUFFER, vbo_id);
-	gl_buffer_data(vbo_id, primitives::quad, GL_STATIC_DRAW);
-	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), (void*)0);
-	glEnableVertexAttribArray(0);
 
+Minimap::Minimap()
+{
+	glGenVertexArrays(1, &vao_id);
+	glBindVertexArray(vao_id);
+	glGenBuffers(1, &vbo_id);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo_id);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(float) * 6 * 2, &primitives::quad[0], GL_STATIC_DRAW);
+	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(glm::vec2), nullptr);
+	glEnableVertexAttribArray(0);
 }
 
-void graphics::Minimap::update(const std::vector<Model>& models, int players)
+void Minimap::update(const std::vector<Model>& models, int players)
 {
 	for (int i = 0; i < players; i++)
 	{
 		this->positions[i] = models[i].get_y_position();
 		std::cout << positions[i] << std::endl;
-
 	}
 }
 
-void graphics::Minimap::render(const Shader& shader) const
+void Minimap::render(const Shader& shader) const
 {
+
 	shader.use();
 	glBindVertexArray(vao_id);
 	glDrawArrays(GL_TRIANGLES, 0, 6);
 	glBindVertexArray(0);
 }
 
-graphics::Minimap::~Minimap()
-{
 }
+
