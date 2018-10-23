829346f8045ef75d12ad77b4401cb42e50465376
Olle@School <olle_sz@hotmail.com>
2018-10-23 16:25:45 +0200

    better settings

--- resources/scripts/setup/settings.lua
+++ resources/scripts/setup/settings.lua
@@ -1,6 +1,21 @@
 settings =
 {
-	resolution = {1280, 720}
-	fullscreen = false
+	window_settings = 
+	{
+		fullscreen = false
+		width = 1280
+		height = 720
+	}
+
+	sound = 
+	{
+		quality = 9.0 -- 10 is max
+	}
+
+	graphics =
+	{
+		debug_fov = 90.0
+		depth_of_field = true --value or true or false
+	}
 }
 
