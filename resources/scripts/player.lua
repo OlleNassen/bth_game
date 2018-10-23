5dbc380930646d2211af1031b1a9592110d9a31b
Olle@School <olle_sz@hotmail.com>
2018-10-23 14:20:06 +0200

    lua is ready

--- resources/scripts/player.lua
+++ resources/scripts/player.lua
@@ -1,7 +1,8 @@
 function setup(entity)
 	--player_playable = true -- 1 should be thought as a playable player 
-	entity.points = 3
-	entity.lol = "hello"
+	entity.points = 0
+	entity.jump_timer = 0.0
+	entity.can_move = true -- instead of playable
 end
 
 function update(delta_seconds, entity)
