458f8e84e0a6a6402f8cf326c10da9edcde03d85
Olle@School <olle_sz@hotmail.com>
2018-10-24 12:26:28 +0200

    IT'S WORKING

--- engine/src/settings.cpp
+++ engine/src/settings.cpp
@@ -5,10 +5,11 @@ Settings::Settings()
 	importer = luaL_newstate();
 	luaL_openlibs(importer);
 
-	if (luaL_loadfile(importer, "../resources/scripts/setup/settings2.lua") || lua_pcall(importer, 0, 0, 0))
+	if (luaL_loadfile(importer, "../resources/scripts/setup/settings.lua") || lua_pcall(importer, 0, 0, 0))
 		std::cout << lua_tostring(importer, -1) << '\n';
 
 	lua_getglobal(importer, "settings");
+	lua_getfield(importer, -1, "window");
 	lua_getfield(importer, -1, "width");
 	lua_getfield(importer, -2, "height");
 
