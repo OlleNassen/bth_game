e4c00652b35f298bf0569ce48a852f21baf54db0
Deadvard <edvardleiborn@telia.com>
2018-10-24 12:08:06 +0200

    code

--- physics/include/rigidbody.hpp
+++ physics/include/rigidbody.hpp
@@ -8,6 +8,8 @@
 namespace physics
 {
 
+static constexpr glm::vec3 gravity{0.0f, -9.82f, 0.0f};
+
 class Rigidbody
 {
 public:
@@ -15,7 +17,22 @@ public:
 	void apply_forces();
 	void solve_constraints(const std::vector<OBB>& constraints);
 
+	void synch_collision_volumes();
+	float inverse_mass();
+	void add_linear_impulse(const glm::vec3& impulse);
+
+	glm::vec3 position;
+	glm::vec3 velocity;
+	glm::vec3 forces;
+	float mass = 1.0f;
+	float restitution = 0.5f;
+	float friction = 0.6f;
+
+	OBB box;
 };
+
+CollisionManifold find_collision_features(Rigidbody& left, Rigidbody& right);
+void apply_impulse(Rigidbody& left, Rigidbody& right, const CollisionManifold& m, int c);
 
 }
 
--- physics/src/rigidbody.cpp
+++ physics/src/rigidbody.cpp
@@ -1,21 +1,119 @@
 #include "rigidbody.hpp"
 
+#include <glm/gtc/constants.hpp>
+
 namespace physics
 {
 
 void Rigidbody::update(float delta_seconds)
 {
+	const float damping = 0.98f;
+	glm::vec3 acceleration = forces * inverse_mass();
+	velocity += acceleration * delta_seconds;
+	velocity *= damping;
 
+	position += velocity * delta_seconds;
+	synch_collision_volumes();
 }
 
 void Rigidbody::apply_forces()
 {
-
+	forces = gravity * mass;
 }
+
 void Rigidbody::solve_constraints(const std::vector<OBB>& constraints)
 {
 
 }
 
+void Rigidbody::synch_collision_volumes()
+{
+	box.position = position;
+}
+
+float Rigidbody::inverse_mass()
+{
+	if (mass == 0.0f)
+		return 0.0f;
+
+	return 1.0f / mass;
+}
+
+void Rigidbody::add_linear_impulse(const glm::vec3& impulse)
+{
+	velocity += impulse;
+}
+
+CollisionManifold find_collision_features(Rigidbody& left, Rigidbody& right)
+{
+	CollisionManifold result;
+	reset_collison_manifold(result);
+	result = find_collision_features(left.box, right.box);
+
+	return result;
+}
+
+void apply_impulse(Rigidbody& left, Rigidbody& right, const CollisionManifold& m, int c)
+{
+	float left_inverse_mass = left.inverse_mass();
+	float right_inverse_mass = right.inverse_mass();
+
+	float inverse_mass_sum = left_inverse_mass + right_inverse_mass;
+
+	if (inverse_mass_sum == 0.0f)
+		return;
+
+	glm::vec3 relative_velocity = right.velocity - left.velocity;
+	glm::vec3 relative_normal = m.normal;
+	relative_normal = glm::normalize(relative_normal);
+
+	if (glm::dot(relative_velocity, relative_normal) > 0.0f)
+		return;
+
+	float e = glm::min(left.restitution, right.restitution);
+	float numerator = (-(1.0f + e) * glm::dot(relative_velocity, relative_normal));
+	float j = numerator / inverse_mass_sum;
+
+	if (m.contacts.size() > 0 && j != 0.0f)
+		j /= m.contacts.size();
+
+	glm::vec3 impulse = relative_normal * j;
+	left.velocity -= impulse * left_inverse_mass;
+	right.velocity += impulse * right_inverse_mass;
+
+	glm::vec3 t = 
+		relative_velocity - 
+		(relative_normal * glm::dot(relative_velocity, relative_normal));
+
+	if (glm::abs(magnitude_squared(t)) < glm::epsilon<float>())
+		return;
+
+	t = glm::normalize(t);
+
+	numerator = -glm::dot(relative_velocity, t);
+	float jt = numerator / inverse_mass_sum;
+
+	if (m.contacts.size() > 0 && jt != 0.0f)
+		jt /= m.contacts.size();
+
+	if (glm::abs(jt) < glm::epsilon<float>())
+		return;
+
+	float friction = glm::sqrt(left.friction * right.friction);
+
+	if (jt > j * friction)
+	{
+		jt = j * friction;
+	}
+	else if (jt < -j * friction)
+	{
+		jt = -j * friction;
+	}
+
+	glm::vec3 tangent_impulse = t * jt;
+		
+	left.velocity -= tangent_impulse * left_inverse_mass;
+	right.velocity += tangent_impulse * right_inverse_mass;
+}
 
 }
