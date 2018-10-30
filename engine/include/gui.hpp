60ce321454c16f251079f45aa3eef45979a44cf7
Deadvard <edvardleiborn@telia.com>
2018-10-30 13:11:54 +0100

    cool menu

--- engine/include/gui.hpp
+++ engine/include/gui.hpp
@@ -68,32 +68,8 @@ private:
 	bool is_debug = false;
 	bool is_on = true;
 	bool want_exit = false;
-	button_array buttons[3];
+	button_array buttons[4];
 	button_array* current_buttons{&buttons[0]};
-
-	std::array<std::string, 12> strings;
-};
-
-class Options
-{
-public:
-	Options();
-	void update(std::chrono::milliseconds delta, const logic::input& i);
-
-	const std::array<std::string, 12>& button_strings() const;
-
-	bool debug() const;
-
-	bool on() const;
-
-	bool exit() const;
-
-private:
-	bool is_debug = false;
-	bool is_on = true;
-	bool want_exit = false;
-	button_array buttons[3];
-	button_array* current_buttons{ &buttons[0] };
 
 	std::array<std::string, 12> strings;
 };
--- engine/src/main.cpp
+++ engine/src/main.cpp
@@ -1,12 +1,7 @@
-//#pragma comment(linker, "/STACK:200000000")
 #include "game.hpp"
-#include <host.hpp>
-//#include <crtdbg.h>
 
 int main()
-{
-	//_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
-	
+{	
 	Game game;
 	game.run();
 	return 0;
