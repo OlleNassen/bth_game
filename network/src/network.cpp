8b395b1450cc3f9d53130ca5c0acc523e9244992
Olle@School <olle_sz@hotmail.com>
2018-10-31 15:28:47 +0100

    hotfix

--- network/src/network.cpp
+++ network/src/network.cpp
@@ -11,7 +11,7 @@ int Messenger::id() const
 
 bool Messenger::connected() const 
 { 
-	return player_host.connected(); //true;
+	return true;// player_host.connected(); //true;
 }
 
 void Messenger::update(GameState& state, const char* ip_address)
