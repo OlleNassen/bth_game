67e7ae462315d2ca91974e9eabcd50d61c3d712a
VINCENT MÅNSSON <vimc15@student.bth.se>
2018-11-01 11:14:30 +0100

    sada

--- resources/scripts/placing_objects.lua
+++ resources/scripts/placing_objects.lua
@@ -1,36 +1,42 @@
 function setup(entity)
-	
+
 end
 
+rotateWasPressed = { false, false, false, false }
+iterator = 1
+
 function update(delta_seconds, entity)
+
 	
 	if entity.button.left
 	then
 		entity.position.x = entity.position.x + (1.0 * delta_seconds)
-		print(entity.position.x)
-	end
-
-	if entity.button.right
+	elseif entity.button.right
 	then
 		entity.position.x = entity.position.x - (1.0 * delta_seconds)
-		print(entity.position.x)
 	end
-
+	
 	if entity.button.up
 	then
 		entity.position.y = entity.position.y + (1.0 * delta_seconds)
-		print(entity.position.y)
-	end
-
-	if entity.button.down
+	elseif entity.button.down
 	then
 		entity.position.y = entity.position.y - (1.0 * delta_seconds)
-		print(entity.position.y)
 	end
 
-	if entity.button.rotate
+	if iterator == 1
 	then
-		print("DO ROTATION!")
+		if entity.button.rotate and not rotateWasPressed[iterator]
+		then
+			print("DO ROTATION!")
+		end
+
+		rotateWasPressed[iterator] = entity.button.rotate
 	end
 
+	iterator = iterator + 1
+	if iterator > 4
+	then
+		iterator = 1
+	end
 end
