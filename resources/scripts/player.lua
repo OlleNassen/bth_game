fda0bfb6622c7afded08e7eb4341bfd96177af34
ADAM BRODD <bradstar95@gmail.com>
2018-11-01 15:51:46 +0100

    Soon good controls will be implemented

--- resources/scripts/player.lua
+++ resources/scripts/player.lua
@@ -2,26 +2,38 @@ function setup(entity)
 	--player_playable = true -- 1 should be thought as a playable player 
 	entity.jump_timer = 1.0
 	entity.can_move = true -- instead of playable
-	entity.in_jump = false
+	entity.maxSpeed = 8000
+	entity.acceleration = 3000
 end
 
 function update(delta_seconds, entity)
 
 	if entity.button.left --and entity.jump_timer < 0.5
 	then
-		entity.forces.x = -4500
+		entity.forces.x = entity.forces.x + (-entity.maxSpeed*entity.acceleration*delta_seconds)
+		--entity.forces.x = -4000
 	end
 
 	if entity.button.right --and entity.jump_timer < 0.5
 	then
-		entity.forces.x = 4500
+		entity.forces.x = entity.forces.x + (entity.maxSpeed*entity.acceleration*delta_seconds)
+		--entity.forces.x = 4000
 	end
 
 	if entity.button.jump and entity.jump_timer < 0
 	then
 		entity.jump_timer = 1.0
-		entity.in_jump = true
-		entity.impulse.y = 28
+		entity.impulse.y = 40
+	end
+
+
+	--Cap the velocity/entity.forces.x
+	if entity.forces.x > entity.maxSpeed
+	then
+		entity.forces.x = entity.maxSpeed
+	elseif entity.forces.x < -entity.maxSpeed
+	then 
+		entity.forces.x = -entity.maxSpeed
 	end
 
 	entity.jump_timer = entity.jump_timer - delta_seconds
