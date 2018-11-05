0269c0b0400bb501b270547ad270a9810db0f03c
Olle@School <olle_sz@hotmail.com>
2018-11-05 13:26:34 +0100

    upload 2

--- resources/scripts/gameloop.lua
+++ resources/scripts/gameloop.lua
@@ -3,8 +3,10 @@ function setup(game)
 	game.points = 0 -- do not change this, edit value in update loop
 	game.max_points = 10
 	--4 playerscores:
+	game.names = {"ole", "ed", "pet", "osby"}
 	game.scores = { 0, 0, 0, 0 }
 	game.finished = { false, false, false, false }
+	game.died = {false, false, false, false}
 	game.clock = 0.0
 	game.winner = false
 end
@@ -13,7 +15,7 @@ function update(delta_seconds, game, entities)
 
 	game.clock = game.clock + delta_seconds
 
-	if game.clock > 60.0 -- Deal out points and reset if timer is too high
+	if game.clock > 5.0 -- Deal out points and reset if timer is too high
 	then
 		while game.points > 0
 		do
@@ -69,6 +71,7 @@ function update(delta_seconds, game, entities)
 		for i = 1, 4, 1
 		do
 			game.finished[i] = false
+			game.died[i] = false
 		end
 
 	end
@@ -94,6 +97,7 @@ function update(delta_seconds, game, entities)
 			game.points = game.points - 1
 
 			game.finished[i] = true
+			game.died[i] = true
 			entities[i].position.x = -2000
 			entities[i].position.y = -2000
 		end
