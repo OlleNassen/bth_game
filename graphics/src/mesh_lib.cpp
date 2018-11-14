c00de954a376db915f573f9d5fa513bf221e8f2e
Elkf <elin.susanna.karlsson@gmail.com>
2018-11-14 13:05:26 +0100

    added chain

--- graphics/src/mesh_lib.cpp
+++ graphics/src/mesh_lib.cpp
@@ -74,7 +74,9 @@ void MeshLib::create_mesh_list()
 		"big_propeller.ssp",				//46
 		"big_fan_shell.ssp",				//47
 		"platform_fence.ssp",               //48
-		"melting_pot.ssp"                   //49
+		"melting_pot.ssp",                  //49
+		"melting_pot.ssp",					//50
+		"chain.ssp"							//51
 	};
 
 	for (auto i = 0u; i < sizeof(meshLoader) / sizeof(meshLoader[0]); ++i)
