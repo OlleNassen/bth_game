a48422c4b4a6e53df40ecf95c8410524ef048540
EMIL HALLIN <emhj16@student.bth.se>
2018-11-15 10:33:08 +0100

    New map with furnace

--- graphics/src/mesh_lib.cpp
+++ graphics/src/mesh_lib.cpp
@@ -76,7 +76,8 @@ void MeshLib::create_mesh_list()
 		"platform_fence.ssp",               //48
 		"melting_pot.ssp",                  //49
 		"hydraulic_press.ssp",              //50
-		"chain.ssp"							//51
+		"chain.ssp",						//51
+		"furnace.ssp"                       //52
 	};
 
 	for (auto i = 0u; i < sizeof(meshLoader) / sizeof(meshLoader[0]); ++i)
