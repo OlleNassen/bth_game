ddbf84ba1c5809035308915b61551c5b26523242
Fu <gimmeyayo@gmail.com>
2018-11-16 15:44:54 +0100

    dfsa

--- graphics/include/overlays.hpp
+++ graphics/include/overlays.hpp
@@ -51,9 +51,13 @@ private:
 	std::chrono::milliseconds death_timer{0};
 	std::chrono::milliseconds loading_timer{0};
 	std::vector<Texture> main_menu;
-	
-	Texture main_menu_1{ "../resources/textures/main_menu_screen/mainmenu_no_battery.png" };
-	Texture main_menu_2{ "../resources/textures/main_menu_screen/mainmenu_low_battery.png" };
+	std::vector<Texture> finish;
+	std::vector<Texture> waiting;
+	std::vector<Texture> death;
+	Texture current_texture;
+
+	/*Texture main_menu_1{ "../resources/textures/main_menu_screen/mainmenu_no_battery.png" };
+	Texture main_menu_2{ "../resources/textures/main_menu_screen/mainmenu_low_battery.png" };*/
 
 	Texture finish_escaped{"../resources/textures/finish_screen/finish_escaped.png"};
 	Texture finish_1_1{"../resources/textures/finish_screen/finish_1.1.png"};
@@ -65,7 +69,7 @@ private:
 	Texture finish_4_1{"../resources/textures/finish_screen/finish_4.1.png"};
 	Texture finish_4_2{"../resources/textures/finish_screen/finish_4.2.png"};
 
-	Texture waiting_1{"../resources/textures/loading_screen/waiting_1.png"};
+	/*Texture waiting_1{"../resources/textures/loading_screen/waiting_1.png"};
 	Texture waiting_2{"../resources/textures/loading_screen/waiting_2.png"};
 	Texture waiting_3{"../resources/textures/loading_screen/waiting_3.png"};
 	Texture waiting_4{"../resources/textures/loading_screen/waiting_4.png"};
@@ -75,7 +79,7 @@ private:
 	Texture death_3{"../resources/textures/death_screen/death_3.png"};
 	Texture death_4{"../resources/textures/death_screen/death_4.png"};
 	Texture death_5{"../resources/textures/death_screen/death_5.png"};
-	Texture death_6{"../resources/textures/death_screen/death_6.png"};
+	Texture death_6{"../resources/textures/death_screen/death_6.png"};*/
 	
 	int player_id;
 	int current_state = 0;
