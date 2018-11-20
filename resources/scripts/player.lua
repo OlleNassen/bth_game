6e801f2ee0e7a595dc34ce1f783a1c95712ef1c4
Deadvard <edvardleiborn@telia.com>
2018-11-20 12:01:00 +0100

    no dt

--- resources/scripts/player.lua
+++ resources/scripts/player.lua
@@ -14,7 +14,7 @@ end
 local gravity = 10000
 
 function update(delta_seconds, entity)
-	entity.update(delta_seconds, entity)
+	entity.update(entity)
 	entity.forces.y = entity.forces.y - gravity
 end
 
@@ -66,18 +66,18 @@ end
 -- states --
 local speed = 8000
 
-idle = function(delta_seconds, entity) 
+idle = function(entity) 
 	move(entity, speed)
 	jump(entity, 0, 15000, entity.anim.start_jump)
 	fall(entity)
 end
 
-jumping = function(delta_seconds, entity) 
+jumping = function(entity) 
 	move(entity, speed * 0.8)
 	fall(entity)
 end
 
-falling = function(delta_seconds, entity) 	
+falling = function(entity) 	
 	move(entity, speed * 0.8)
 	
 	if entity.anim.current == entity.anim.hanging_left then
@@ -94,11 +94,11 @@ falling = function(delta_seconds, entity)
 	end
 end
 
-landing = function(delta_seconds, entity) 
+landing = function(entity) 
 	move(entity, speed * 0.8)
 end
 
-hanging_left = function(delta_seconds, entity) 
+hanging_left = function(entity) 
 	jump(entity, 2000, 2000, entity.anim.jump_from_wall)
 
 	if entity.anim.current == entity.anim.idle then
@@ -106,7 +106,7 @@ hanging_left = function(delta_seconds, entity)
 	end
 end
 
-hanging_right = function(delta_seconds, entity) 
+hanging_right = function(entity) 
 	jump(entity, -2000, 2000, entity.anim.jump_from_wall)
 
 	if entity.anim.current == entity.anim.idle then
@@ -114,6 +114,6 @@ hanging_right = function(delta_seconds, entity)
 	end
 end
 
-sliding = function(delta_seconds, entity) 
+sliding = function(entity) 
 
 end
