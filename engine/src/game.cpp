8d35c0ca9130a1e7100787b8d8dde5c950c89e68
VINCENT MÅNSSON <vimc15@student.bth.se>
2018-11-21 15:28:11 +0100

    teadf

--- engine/src/game.cpp
+++ engine/src/game.cpp
@@ -284,11 +284,11 @@ void Game::update(std::chrono::milliseconds delta)
 			for (int i = 0; i < static_cast<int>(player_count); i++)
 			{
 				int obj_id = dynamics[i].player_moving_object_id;
-				int obj_type_id = dynamics[i].objects_type_id;
+				int obj_type_id = dynamics[i].player_moving_object_type_id;
 				if (obj_id != -1 && obj_type_id != -1)
 				{
 					collision_data data;
-					int m_id = level.add_object(data, dynamics[i].objects_type_id);
+					int m_id = level.add_object(data, obj_type_id);
 					int d_id = physics.add_dynamic_body(dynamics[obj_id].position, { 0, 0 }, data.width, data.height, { 0, 0 }, placed_objects_list_id);
 
 					std::cout << "Model ID:\t" << m_id << "\nDynamic ID:\t" << d_id << std::endl << std::endl;
@@ -302,9 +302,6 @@ void Game::update(std::chrono::milliseconds delta)
 					dynamics[d_id].model_id = m_id;
 					dynamics[d_id].objects_type_id = data.objects_type_id;
 					dynamics[d_id].place_state = 0;
-
-					dynamics[i].player_moving_object_type_id = data.objects_type_id;
-					dynamics[i].player_moving_object_id = d_id;
 
 					players_placed_objects_id[i] = { dynamics[d_id].dynamic_id, dynamics[d_id].model_id,
 								dynamics[d_id].place_state, dynamics[d_id].objects_type_id };
