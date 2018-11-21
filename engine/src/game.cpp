72e066dcc4abe1551ddc433f46fc0efa7aa75e73
VINCENT MÅNSSON <vimc15@student.bth.se>
2018-11-21 16:02:55 +0100

    fixed error

--- engine/src/game.cpp
+++ engine/src/game.cpp
@@ -316,7 +316,7 @@ void Game::update(std::chrono::milliseconds delta)
 		
 		for (int i = 0; i < static_cast<int>(player_count); i++)
 		{
-			if (players_placed_objects_id[i].place_state != 2 || dynamics[players_placed_objects_id[i].dynamics_id].place_state != 2)
+			if (players_placed_objects_id[i].place_state != 2)
 			{
 				level.moving_models[players_placed_objects_id[i].model_id].set_position(dynamics[players_placed_objects_id[i].dynamics_id].position);
 
