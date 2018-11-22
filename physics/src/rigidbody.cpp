2f3ecb25194115ee6f2fd4b2fa19da31f49536b5
Olle@School <olle_sz@hotmail.com>
2018-11-22 16:19:51 +0100

    +1

--- physics/src/rigidbody.cpp
+++ physics/src/rigidbody.cpp
@@ -7,12 +7,11 @@ namespace physics
 
 void Rigidbody::update(float delta_seconds)
 {
-	const float damping = 0.9f;
-	const float max_velocity = 75.0f;
+	const float damping = 0.99f;
 	glm::vec3 acceleration = forces * inverse_mass;
 	velocity += acceleration * delta_seconds;
-	velocity.x = glm::clamp(velocity.x * damping, -max_velocity, max_velocity);
-	velocity.y = glm::clamp(velocity.y * damping, -max_velocity, max_velocity);
+	velocity *= damping;
+
 	position += velocity * delta_seconds;
 	synch_collision_volumes();
 }
