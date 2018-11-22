ff7109ac020d30f4c893f9c38f083ca7f3aee5ed
Olle@School <olle_sz@hotmail.com>
2018-11-22 16:27:19 +0100

    olle

--- engine/src/game.cpp
+++ engine/src/game.cpp
@@ -277,7 +277,7 @@ void Game::update(std::chrono::milliseconds delta)
 				int m_id = level.add_object(data, placed_objects_list_id);
 				int d_id = physics.add_dynamic_body(start_position, { 0, 0 }, data.width, data.height, { 0, 0 }, placed_objects_list_id);
 
-				std::cout << "Model ID:\t" << m_id << "\nDynamic ID:\t" << d_id << "Type ID:\t" << placed_objects_list_id << std::endl << std::endl;
+				//std::cout << "Model ID:\t" << m_id << "\nDynamic ID:\t" << d_id << "Type ID:\t" << placed_objects_list_id << std::endl << std::endl;
 
 				dynamics[d_id].position = start_position;
 				dynamics[d_id].velocity = { 0.0f, 0.0f };
@@ -318,7 +318,7 @@ void Game::update(std::chrono::milliseconds delta)
 					int m_id = level.add_object(data, obj_type_id);
 					int d_id = physics.add_dynamic_body(dynamics[obj_id].position, { 0, 0 }, data.width, data.height, { 0, 0 }, obj_type_id);
 
-					std::cout << "Model ID:\t" << m_id << "\nDynamic ID:\t" << d_id << "\nType ID:\t" << obj_type_id << std::endl << std::endl;
+					//std::cout << "Model ID:\t" << m_id << "\nDynamic ID:\t" << d_id << "\nType ID:\t" << obj_type_id << std::endl << std::endl;
 
 					//dynamics[d_id].position = start_position;
 					//dynamics[d_id].velocity = { 0.0f, 0.0f };
@@ -355,12 +355,6 @@ void Game::update(std::chrono::milliseconds delta)
 					players_placed_objects_id[i].place_state = 0;
 
 
-			}
-			else
-			{
-				glm::vec3 pos = physics.get_closest_wall_point(players_placed_objects_id[i].dynamics_id);
-				dynamics[players_placed_objects_id[i].dynamics_id].position = { pos.x, pos.y };
-				level.moving_models[players_placed_objects_id[i].model_id].set_position({ pos.x, pos.y });
 			}
 		}
 
@@ -406,6 +400,12 @@ void Game::update(std::chrono::milliseconds delta)
 					dynamics[index].dynamic_id = ppoi.dynamics_id;*/
 
 					total_nr_objects--;
+				}
+				else
+				{
+					glm::vec3 pos = physics.get_closest_wall_point(players_placed_objects_id[i].dynamics_id);
+					dynamics[players_placed_objects_id[i].dynamics_id].position = { pos.x, pos.y };
+					level.moving_models[players_placed_objects_id[i].model_id].set_position({ pos.x, pos.y });
 				}
 			}
 
