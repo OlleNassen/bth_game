57049f96cee7a25c9f3668c90ba71c23ad2b03ed
Olle@School <olle_sz@hotmail.com>
2018-12-04 10:26:17 +0100

    add

--- graphics/src/renderer.cpp
+++ graphics/src/renderer.cpp
@@ -6,10 +6,81 @@
 #include <glm/gtc/matrix_transform.hpp>
 #include <iomanip>
 
+#include <d3d11_4.h>
+#include <dxgi1_6.h>
+#pragma comment(lib, "dxgi.lib")
+
+#include <psapi.h>
+
+#include <string>
+
 namespace graphics
 {
 
 using namespace std::chrono_literals;
+
+void vramUsage()
+{
+	IDXGIFactory* dxgifactory = nullptr;
+	HRESULT ret_code = ::CreateDXGIFactory(
+		__uuidof(IDXGIFactory),
+		reinterpret_cast<void**>(&dxgifactory));
+
+	if (SUCCEEDED(ret_code))
+	{
+		IDXGIAdapter* dxgiAdapter = nullptr;
+
+		if (SUCCEEDED(dxgifactory->EnumAdapters(0, &dxgiAdapter)))
+		{
+			IDXGIAdapter4* dxgiAdapter4 = NULL;
+			if (SUCCEEDED(dxgiAdapter->QueryInterface(__uuidof(IDXGIAdapter4), (void**)&dxgiAdapter4)))
+			{
+				DXGI_QUERY_VIDEO_MEMORY_INFO info;
+
+				if (SUCCEEDED(dxgiAdapter4->QueryVideoMemoryInfo(0, DXGI_MEMORY_SEGMENT_GROUP_LOCAL, &info)))
+				{
+					float memoryUsage = float(info.CurrentUsage / 1024.0 / 1024.0); //MiB
+
+					char msg[100];
+					sprintf_s(msg, "%.2f MiB used", memoryUsage);
+					MessageBoxA(0, msg, "VRAM", 0);
+				};
+
+				dxgiAdapter4->Release();
+			}
+			dxgiAdapter->Release();
+		}
+		dxgifactory->Release();
+	}
+}
+
+void ramUsage()
+{
+	//src: https://docs.microsoft.com/en-us/windows/desktop/api/psapi/ns-psapi-_process_memory_counters
+
+	DWORD currentProcessID = GetCurrentProcessId();
+
+	HANDLE hProcess = OpenProcess(PROCESS_QUERY_INFORMATION | PROCESS_VM_READ, FALSE, currentProcessID);
+
+	if (NULL == hProcess)
+		return;
+
+	PROCESS_MEMORY_COUNTERS pmc{};
+	if (GetProcessMemoryInfo(hProcess, &pmc, sizeof(pmc)))
+	{
+		//PagefileUsage is the:
+			//The Commit Charge value in bytes for this process.
+			//Commit Charge is the total amount of memory that the memory manager has committed for a running process.
+
+		float memoryUsage = float(pmc.PagefileUsage / 1024.0 / 1024.0); //MiB
+
+		char msg[100];
+		sprintf_s(msg, "%.2f MiB committed", memoryUsage);
+		MessageBoxA(0, msg, "RAM", 0);
+	}
+
+	CloseHandle(hProcess);
+}
 
 ModelsToRender::ModelsToRender(const Model& player, const Model* begin, const Model* end)
 {
@@ -482,6 +553,9 @@ void Renderer::render(
 		
 		glEnable(GL_DEPTH_TEST);
 	}
+
+	vramUsage();
+	ramUsage();
 
 }
 
