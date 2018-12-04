8ff0f2bdee759fad5a95dcd1e42a6fd700b83ea5
VINCENT MÅNSSON <vimc15@student.bth.se>
2018-12-04 10:13:25 +0100

    fix

--- engine/src/game.cpp
+++ engine/src/game.cpp
@@ -300,7 +300,7 @@ void Game::update(std::chrono::milliseconds delta)
 			for (int i = 0; i < static_cast<int>(player_count); i++)
 			{
 				glm::vec2 start_position = start_positons[random_position[i]];
-				placed_objects_list_id = 0; //random_picked_object();
+				placed_objects_list_id = random_picked_object();
 				collision_data data;
 				int m_id = level->add_object(data, placed_objects_list_id);
 				int d_id = physics.add_dynamic_body(start_position, { 0, 0 }, data.width, data.height, { 0, 0 }, placed_objects_list_id);
