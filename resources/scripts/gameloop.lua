e176c2806c0eff34ad868390af8f54d9555d09f4
Olle@School <olle_sz@hotmail.com>
2018-12-04 13:28:48 +0100

    lower

--- resources/scripts/gameloop.lua
+++ resources/scripts/gameloop.lua
@@ -227,7 +227,7 @@ function update(delta_seconds, game, entities, player_count)
 	do
 		if entities[i].dash_active and entities[i].triggered < 4 and entities[i].triggered ~= -1 
 		then
-			entities[entities[i].triggered + 1].impulse.x = entities[i].velocity.x * 1000
+			entities[entities[i].triggered + 1].impulse.x = entities[i].velocity.x * 10
 
 			print("DASSSSSSH")
 			--right.position.x = -10000
