ea947e6c45c956c2b5abc05134d4ac64194cb8ba
Fu <gimmeyayo@gmail.com>
2018-12-05 11:50:21 +0100

    sfda

--- resources/scripts/player.lua
+++ resources/scripts/player.lua
@@ -196,7 +196,6 @@ function update_control(delta_seconds, entity)
 
 			entity.impulse.y = 46
 			entity.can_jump = false
-			entity.jumped_last = true
 
 		end
 		
@@ -262,7 +261,6 @@ function update_control(delta_seconds, entity)
 		then
 			entity.impulse.y = 48
 			entity.can_jump = false
-			entity.jumped_last = true
 		end
 
 		if entity.button.left
@@ -389,8 +387,10 @@ function update_control(delta_seconds, entity)
 		end
 	end
 
-	if entity.button.jump == false
+	if entity.button.jump 
 		then
+			entity.jumped_last = true
+		else
 			entity.jumped_last = false
 	end
 
