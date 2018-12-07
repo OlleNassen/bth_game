48cd0129242f8b0f3139bb324b40bb2b86c762d5
Olle@School <olle_sz@hotmail.com>
2018-12-07 09:31:33 +0100

    better

--- resources/scripts/gameloop.lua
+++ resources/scripts/gameloop.lua
@@ -238,12 +238,12 @@ function update(delta_seconds, game, entities, player_count)
 		then
 			if entities[i].velocity.x  > 0 and not game.immune[i]
 			then
-				entities[entities[i].triggered + 1].impulse.x = 20
+				entities[entities[i].triggered + 1].impulse.x = 50
 				game.immune[i] = true
 
 			elseif entities[i].velocity.x < 0 and not game.immune[i]
 			then
-				entities[entities[i].triggered + 1].impulse.x = -20
+				entities[entities[i].triggered + 1].impulse.x = -50
 				game.immune[i] = true
 			end
 			--entities[entities[i].triggered + 1].impulse.x = entities[i].velocity.x * 10
