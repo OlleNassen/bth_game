3b449a3dbaaa7507b31e82407b794d62fec00865
Olle@School <olle_sz@hotmail.com>
2018-12-17 10:59:07 +0100

    added ram checks that are commented out, added winmain without console and fullscreen

--- engine/src/main.cpp
+++ engine/src/main.cpp
@@ -6,3 +6,10 @@ int main()
 	game.run();
 	return 0;
 }
+
+int WINAPI WinMain(HINSTANCE hThisInstance, HINSTANCE hPrevInstance, LPSTR lpszArgument, int nCmdShow)
+{
+	Game game;
+	game.run();
+	return 0;
+}
